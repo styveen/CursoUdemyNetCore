27f3b7987fe797ea4853ffb56eb81656bc53da5a
David Styveen <dstyveen@gmail.com>
2020-05-13 18:10:48 -0300

    first commit

---
+++ src/Api.Application/Startup.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Api.CrossCuting.DependencyInjection;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Rewrite;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Logging;
+using Microsoft.Extensions.Options;
+using Microsoft.OpenApi.Models;
+
+namespace Application
+{
+    public class Startup
+    {
+        public Startup(IConfiguration configuration)
+        {
+            Configuration = configuration;
+        }
+
+        public IConfiguration Configuration { get; }
+
+        // This method gets called by the runtime. Use this method to add services to the container.
+        public void ConfigureServices(IServiceCollection services)
+        {
+            ConfigureService.ConfigureDependenciesService(services);
+            ConfigureRepository.ConfigureDependenciesRepository(services);
+
+            services.AddSwaggerGen(c =>
+            {
+                c.SwaggerDoc("v1", new OpenApiInfo
+                {
+                    Title = "Curso AspNetCore 2.2",
+                    Version = "v1",
+                    Description = "Exemplo de API REST criada com o ASP.NET Core",
+                    Contact = new OpenApiContact
+                    {
+                        Name = "David Styveen",
+                        Url = new Uri("https://github.com/styveen/")
+                    }
+                });
+            });
+
+            services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_2_2);
+        }
+
+        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+        public void Configure(IApplicationBuilder app, IHostingEnvironment env)
+        {
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+            }
+
+            app.UseSwagger();
+            app.UseSwaggerUI(c =>
+            {
+                c.RoutePrefix = string.Empty;
+                c.SwaggerEndpoint("/swagger/v1/swagger.json", "Curso de API com AspNetCore 2.2");
+            });
+
+            var option = new RewriteOptions();
+            option.AddRedirect("^$", "swagger");
+            app.UseRewriter(option);
+
+            app.UseMvc();
+        }
+    }
+}
---
+++ src/Api.CrossCuting/DependencyInjection/ConfigureService.cs
@@ -0,0 +1,15 @@
+using Api.Domain.Interfaces.Services.User;
+using Api.Service.Services;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace Api.CrossCuting.DependencyInjection
+{
+    public class ConfigureService
+    {
+        public static void ConfigureDependenciesService(IServiceCollection serviceCollection)
+        {
+            serviceCollection.AddTransient<IUserService, UserService>();
+
+        }
+    }
+}
